504631167a675ab2510b210614628c704ac37fcc
zsijtt <jean-luc.thirot@vd.ch>
2020-01-07 15:05:53 +0100

    [TASK] add TCA and sql for tcafe work in progess

---
+++ Configuration/TCA/tx_tcafe_record.php
@@ -0,0 +1,250 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+
+return [
+    'ctrl' => [
+        'title' => 'tcafe records',
+        'descriptionColumn' => 'notes',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'versioningWS' => true,
+        'origUid' => 't3_origuid',
+        'editlock' => 'editlock',
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'translationSource' => 'l10n_source',
+        'delete' => 'deleted',
+        'enablecolumns' => [
+
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+            'fe_group' => 'fe_group',
+        ],
+        'searchFields' => 'uid,title',
+    ],
+    'interface' => [
+        'showRecordFieldList' => 'cruser_id,pid,sys_language_uid,l10n_parent,l10n_diffsource,hidden,starttime,endtime,fe_group,title,bodytext,datetime,url,file_url,relation_categories,relation_csv,relation_many,relation_fal,relation_inline'
+    ],
+    'columns' => [
+
+
+
+        'title' => [
+            'exclude' => false,
+            'l10n_mode' => 'prefixLangTitle',
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel',
+            'config' => [
+                'type' => 'input',
+                'size' => 60,
+                'eval' => 'required',
+            ]
+        ],
+        'bodytext' => [
+            'exclude' => false,
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel',
+            'config' => [
+                'type' => 'text',
+                'cols' => 30,
+                'rows' => 5,
+                'softref' => 'rtehtmlarea_images,typolink_tag,images,email[subst],url',
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+            ]
+        ],
+        'datetime' => [
+            'exclude' => false,
+            'label' => 'Date time',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'size' => 16,
+                'eval' => 'datetime,int',
+            ]
+        ],
+        'url' => [
+            'exclude' => false,
+            'label' => 'Url to page',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputLink',
+                'size' => 30,
+                'max' => 255,
+                'eval' => 'trim,required',
+                'softref' => 'typolink'
+            ]
+        ],
+        'file_url' => [
+            'exclude' => false,
+            'label' => 'Url to file',
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'eval' => 'required',
+                'softref' => 'typolink'
+            ]
+        ],
+        'relation_categories' => [
+            'exclude' => true,
+            'label' => 'Relation categories bi directional and inline',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectTree',
+                'treeConfig' => [
+                    'dataProvider' => \GeorgRinger\News\TreeProvider\DatabaseTreeDataProvider::class,
+                    'parentField' => 'parent',
+                    'appearance' => [
+                        'showHeader' => true,
+                        'expandAll' => true,
+                        'maxLevels' => 99,
+                    ],
+                ],
+                'MM' => 'sys_category_record_mm',
+                'MM_match_fields' => [
+                    'fieldname' => 'categories',
+                    'tablenames' => 'tx_news_domain_model_news',
+                ],
+                'MM_opposite_field' => 'items',
+                'foreign_table' => 'sys_category',
+                'foreign_table_where' => ' AND (sys_category.sys_language_uid = 0 OR sys_category.l10n_parent = 0) ORDER BY sys_category.sorting',
+                'size' => 10,
+                'minitems' => 0,
+                'maxitems' => 99,
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ]
+        ],
+        'relation_many' => [
+            'exclude' => true,
+            'label' => 'Relation many to many',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'tx_news_domain_model_news',
+                'foreign_table' => 'tx_news_domain_model_news',
+                'MM_opposite_field' => 'related_from',
+                'size' => 5,
+                'minitems' => 0,
+                'maxitems' => 100,
+                'MM' => 'tx_news_domain_model_news_related_mm',
+                'suggestOptions' => [
+                    'default' => [
+                        'suggestOptions' => true,
+                        'addWhere' => ' AND tx_news_domain_model_news.uid != ###THIS_UID###'
+                    ]
+                ],
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ]
+        ],
+        'relation_inline' => [
+            'exclude' => false,
+            'label' => 'Relation inline to tt_content',
+            'config' => [
+                'type' => 'inline',
+                'allowed' => 'tt_content',
+                'foreign_table' => 'tt_content',
+                'foreign_sortby' => 'sorting',
+                'foreign_field' => 'tx_news_related_news',
+                'minitems' => 0,
+                'maxitems' => 99,
+                'appearance' => [
+                    'useXclassedVersion' => $configuration->getContentElementPreview(),
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'levelLinksPosition' => 'bottom',
+                    'useSortable' => true,
+                    'showPossibleLocalizationRecords' => true,
+                    'showRemovedLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                    'enabledControls' => [
+                        'info' => false,
+                    ]
+                ],
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ]
+        ],
+        'relation_fal' => [
+            'exclude' => true,
+            'label' => 'Relation FAL',
+            'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+                'fal_media',
+                [
+                    'behaviour' => [
+                        'allowLanguageSynchronization' => true,
+                    ],
+                    'appearance' => [
+                        'createNewRelationLinkTitle' => 'File',
+                        'showPossibleLocalizationRecords' => true,
+                        'showRemovedLocalizationRecords' => true,
+                        'showAllLocalizationLink' => true,
+                        'showSynchronizationLink' => true
+                    ],
+                    'foreign_match_fields' => [
+                        'fieldname' => 'fal_media',
+                        'tablenames' => 'tx_news_domain_model_news',
+                        'table_local' => 'sys_file',
+                    ],
+
+                ]
+            )
+        ],
+    ],
+    'types' => [
+        // default news
+        '0' => [
+            'showitem' => '
+                    --palette--;;paletteBase,title,--palette--;;paletteSlug,teaser,
+                    --palette--;;paletteDate,
+                    bodytext,
+                    
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    relation_categories,
+                          
+                --div--;Relations,
+                    relation_many,relation_inline,relation_fal,
+
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;paletteLanguage,
+                    
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    --palette--;;paletteHidden,
+                    --palette--;;paletteAccess,
+
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.extended,'
+        ],
+    ],
+    'palettes' => [
+        'paletteBase' => [
+            'label' => 'Champs de base',
+            'showitem' => 'datetime,archive,',
+        ],
+        'paletteHidden' => [
+            'showitem' => 'hidden',
+        ],
+        'paletteAccess' => [
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access',
+            'showitem' => '
+                starttime;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:starttime_formlabel,
+                endtime;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:endtime_formlabel,
+                --linebreak--,
+                fe_group;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:fe_group_formlabel,
+                --linebreak--,editlock
+            ',
+        ],
+        'paletteLanguage' => [
+            'showitem' => '
+                sys_language_uid;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:sys_language_uid_formlabel,l10n_parent, l10n_diffsource,
+            ',
+        ],
+
+    ]
+];
---
+++ ext_tables.sql
@@ -0,0 +1,44 @@
+#
+# Table structure for table 'tx_tcafe_record'
+#
+CREATE TABLE tx_tcafe_record (
+	title tinytext,
+	bodytext mediumtext,
+	datetime bigint(20) DEFAULT '0' NOT NULL,
+	url text,
+	file_url text,
+	relation_categories int(11) DEFAULT '0' NOT NULL,
+	relation_csv tinytext,
+	relation_many int(11) DEFAULT '0' NOT NULL,
+	relation_fal int(11) unsigned DEFAULT '0',
+	relation_inline int(11) DEFAULT '0' NOT NULL,
+);
+
+
+#
+# Extend table structure of table 'relation_csv'
+#
+CREATE TABLE tx_tcafe_relation_csv (
+	title tinytext,
+);
+
+
+#
+# Table structure for table 'relation_many'
+#
+#
+CREATE TABLE tx_tcafe_record_relation_many_mm (
+	uid_local int(11) DEFAULT '0' NOT NULL,
+	uid_foreign int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
+);
+
+# relation_inline
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content (
+	tx_tcafe_relation_inline int(11) DEFAULT '0' NOT NULL,
+);
+
