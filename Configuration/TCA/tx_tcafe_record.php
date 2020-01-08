7f1bcd6ae02477dc7b5676fa46998fcc78e34dba
s8t9co <christian.bellet@vd.ch>
2020-01-08 10:18:06 +0100

    [TASK] Include generic TCA and restore partial TCA before call to generic fields

---
+++ Configuration/TCA/tx_tcafe_record.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$ll = 'LLL:EXT: ' . \Vd\Tcafe\Utility\ConfigurationUtility::EXT_KEY . '/Resources/Private/Language/locallang_db.xlf:';
+
+// To be overwritten
+return [];
