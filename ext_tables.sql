fc24454b5c82815ca6398deb8d5cef22dc9e1b7e
= <jeremie.roulin@vd.ch>
2020-01-16 13:51:57 +0100

    [TASK] Code cleanup

--- ext_tables.sql
+++ ext_tables.sql
@@ -62,10 +62,3 @@ CREATE TABLE tx_tcafe_record_relation_mm (
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign),
 );
-
-#
-# Table structure for table 'tx_tcafe_record_relation_mm'
-#
-CREATE TABLE tx_news_domain_model_news (
-	select_single_3 tinytext,
-);
